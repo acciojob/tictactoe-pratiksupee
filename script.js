e238ff664eb85d1661a2cbdd788ad59e3810fb64
pratiksupee <135314162+pratiksupee@users.noreply.github.com>
2026-04-02 20:09:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 const submitBtn = document.getElementById('submit');
-const player1Input = document.getElementById('player-1');
-const player2Input = document.getElementById('player-2');
+const player1Input = document.getElementById('player1');
+const player2Input = document.getElementById('player2');
 const setupDiv = document.getElementById('setup');
 const gameBoardDiv = document.getElementById('game-board');
 const messageDiv = document.querySelector('.message');
